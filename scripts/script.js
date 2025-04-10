5fb96f48b3f994d7d3d272906265a927eff915f8
Debesh <debesh391@gmail.com>
2025-04-10 13:46:47 +0530

    version update

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
 const responseField = document.querySelector('.response-box');
 const inputBox = document.querySelector('.input-box');
-const submitBtn = document.querySelector('.submit-btn');
-
+const submitBtn = document.querySelector('.send-btn');
+const micElement = document.querySelector('.mic-btn');
 
 const apiUrl = 'https://ai-model-debesh.onrender.com/question=';
-
-
 
 function createQuestion(question) {
     return `${apiUrl+question}`;
@@ -37,17 +35,18 @@ async function loadAnimation() {
 
 function printWordByWord(text, delay, elementId) {
     const words = text.split(' ');
-    let index = 0;
-    const element = document.getElementById(elementId);
-  
+    let index = 0;   
+    
     function printNextWord() {
       if (index < words.length) {
-        element.innerHTML += words[index] + ' '; // Add the word to the HTML element
+        elementId.innerHTML += words[index] + ' '; // Add the word to the HTML element
         index++;
         setTimeout(printNextWord, delay);
       }
     }
     printNextWord();
+
+
 }
   
 //   const text = "Hello, this is an example of printing word by word!";
@@ -56,9 +55,7 @@ function printWordByWord(text, delay, elementId) {
 
 let onGoing = false;
 
-
 submitBtn.addEventListener('click', async()=>{
-  
   if(responseField.classList.contains('response-box-style')) {
     responseField.classList.remove('response-box-style') 
   }
@@ -68,12 +65,13 @@ submitBtn.addEventListener('click', async()=>{
       submitBtn.classList.add('disable-btn');
       responseField.innerHTML = '';
       const question = inputBox.value;
+      inputBox.value = '';
       const res = await getResponse(question); 
       const totalDelay = res.length;
 
       responseField.classList.remove('loader')
-      responseField.classList.add('response-box-style')   
-      printWordByWord(res,100,'response-box')
+      responseField.classList.add('response-box-style')  
+      printWordByWord(res,100,responseField)
       setTimeout(()=>{
         submitBtn.classList.remove('disable-btn');
         onGoing = false;
@@ -82,4 +80,47 @@ submitBtn.addEventListener('click', async()=>{
   }
     
     //console.log(res)
+})
+/////// Voice Recognition Code::::::::::::::::
+
+
+async function startRecognition() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  if (!SpeechRecognition) {
+    alert('Speech Recognition not supported in this browser.');
+    return;
+  }
+
+  const recognition = new SpeechRecognition();
+  recognition.lang = 'en-US';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  recognition.start();
+
+  recognition.onresult = async (event) => {
+    const transcript = event.results[0][0].transcript;
+    const speechRes = await getResponse(transcript)
+    console.log(speechRes);
+    
+    // Speak it back
+    const botVoice = speechSynthesis.getVoices()[4];
+    
+    const utterance = new SpeechSynthesisUtterance(speechRes);  
+    utterance.voice = botVoice;
+    speechSynthesis.speak(utterance);
+    responseField.innerHTML = '';
+    printWordByWord(speechRes,100,responseField);
+  };
+
+  recognition.onerror = (event) => {
+    responseField.innerHTML = 'Error: ' + event.error;
+  };
+}
+window.speechSynthesis.onvoiceschanged = () => {
+  speechSynthesis.getVoices();
+};
+micElement.addEventListener('click', ()=>{
+    startRecognition();
 })
